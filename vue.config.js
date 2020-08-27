a9b060a24ae35c1224f2f45880d733d364fe92e3
abearxiong pc <xiongxiao1012@outlook.com>
2020-08-27 16:36:13 +0800

    docs: change docs file

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 // vue.config.js
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-    ? '.'
-    : '.'
+    ? './'
+    : './',
+    devServer: {
+        disableHostCheck: true,
+        // host: '192.168.3.10',   // 通过ipconfig查看本机IP
+        port: 8081,
+    }
 }

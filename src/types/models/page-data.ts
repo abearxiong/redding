4e8c6ccb1491ef0ec2f40307484e7ae10bb9da8a
abearxiong pc <xiongxiao1012@outlook.com>
2020-08-28 15:35:27 +0800

    docs: record 15：35

--- src/types/models/page-data.ts
+++ src/types/models/page-data.ts
@@ -1,8 +1,14 @@
 enum DataType {
+    // Api,
+    Attach,
     Audio,
+    Bookmark,
+    Code,
+    Formula, // 公式
     Markdown,
     PageLink,
     Picture,
+    SplitLine,
     Text,
     Video,
 }
@@ -12,15 +18,22 @@ enum DataCategory {
     Comment
 }
 interface IPageData extends IBaseData{
-    hash?: string
+    hash?: string,
+    createTime: number,
+    updateTime: number,
 }
 
 abstract class PageData extends BaseData implements IPageData{
     public hash: string;
+    createTime: number;
+    updateTime: number;
     public constructor(pageData: IPageData){
         super(pageData)
         const {hash} = pageData;
         this.hash = hash??"";
+        this.createTime = 1;
+        this.updateTime = 1;
     }
+
     public abstract setHash():void;
 }
